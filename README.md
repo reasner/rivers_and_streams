b6b62f4b36f06627b3e9f79091d15325050e9c33
reasner <msreasner@gmail.com>
2021-01-19 10:40:05 -0500

    output and cleanup

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # rivers_and_streams
 Create a measure of rivers and streams by geographical boundaries in the continental United States
+
+
+only named rivers and streams in order to only county each river/stream once per county
+
+
+in base case .length returns lengths in meters that are roughly equivalent
+to mileage given in raw data (i.e. total length in 'miles' of named rivers
+in IN is 8547.95, whereas total length in meters using .length is 13726886.76
+which is equivalent to 8529.49 miles
+the ratio of .length to raw data total is: 8529.49/8547.95 = 0.9978
+ratio for IN once split and calculated by county is 13866.08km == 8615.98 miles
+which represnets and over reporting of 1.00795, less than 1%
--- rivers_and_streams.py
+++ rivers_and_streams.py
@@ -22,12 +22,6 @@ rivers_and_streams = gpd.read_file(rivers_and_streams_shapefile_path)
 rivers_and_streams = rivers_and_streams.to_crs("EPSG:5070")
 rivers_and_streams = rivers_and_streams[~(rivers_and_streams['State'] == 'AK') & ~(rivers_and_streams['State'] == 'HI') & ~(rivers_and_streams['State'] == 'PR')]
 
-'''
-#temporary indiana only to start
-rivers_and_streams = rivers_and_streams[(rivers_and_streams['State'] == 'IN')]
-county_map['state'] = county_map['fips'].str[:2]
-county_map = county_map[(county_map['state'] == '18')]
-'''
 
 #Collect river and stream data by county
 rivers_and_streams = rivers_and_streams[rivers_and_streams['Name'].notna()]
@@ -41,14 +35,6 @@ for loc in fips:
     to_fips.append(str(loc))
     number_of_rivers_and_streams.append(len(keep['Name'].unique()))
     length_of_rivers_and_streams.append((keep.length.sum()/1000).round(2))
-    #in base case .length returns lengths in meters that are roughly equivalent
-    #to mileage given in raw data (i.e. total length in 'miles' of named rivers
-    #in IN is 8547.95, whereas total length in meters using .length is 13726886.76
-    #which is equivalent to 8529.49 miles
-    #the ratio of .length to raw data total is: 8529.49/8547.95 = 0.9978
-    #ratio for IN once split and calculated by county is 13866.08km == 8615.98 miles
-    #which represnets and over reporting of 1.00795, less than 1%
-    print(str(loc))
 
 #Output by county data to .csv
 river_data = pd.DataFrame(
@@ -66,11 +52,8 @@ rivers_and_streams.plot(ax=ax,facecolor="none",linewidth=0.1,edgecolor='blue')
 county_map.plot(ax=ax,facecolor="none",linewidth=0.1,edgecolor='gray')
 
 rivers_and_streams_img_path = os.path.join(cd,r'rivers_and_streams.png')
-plt.savefig(rivers_and_streams_img_path,bbox_inches='tight',pad_inches=0,dpi=100)
+plt.savefig(rivers_and_streams_img_path,bbox_inches='tight',pad_inches=0,dpi=300)
 
-
-
-#only named rivers and streams in order to only county each river/stream once per county
 
 
 
